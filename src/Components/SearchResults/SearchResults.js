a83ed01cef8f31f14b83ab2095482a3cdfaf688d
dwyrwas <djwyrwas@gmail.com>
2022-06-19 20:36:45 -0400

    added search results

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class SearchResults extends React.Component{
+    render(){
+        <div className="SearchResults">
+            <h2>Results</h2>
+  <!-- Add a TrackList component -->
+            </div>
+    }
+}
