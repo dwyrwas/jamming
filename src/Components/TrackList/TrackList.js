1357c4a1a0373da3d21967450a62d8dd27dd8a1a
dwyrwas <djwyrwas@gmail.com>
2022-06-20 16:20:28 -0400

    added playlist rendering

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -4,12 +4,13 @@ import './TrackList.css';
 
 class TrackList extends React.Component{
     render() {
-        
         return(
         <div className='TrackList'>
-           <Track/>
-           <Track/>
-           <Track/>
+            {
+                this.props.tracks.map(track => {
+                    return <Track track={track} key={track.id} />
+                })
+            }
         </div>);
     }
 }
