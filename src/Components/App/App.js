eaa406043ce62b0309be19d653cd48ea2f704343
dwyrwas <djwyrwas@gmail.com>
2022-06-20 16:54:30 -0400

    addTrack method

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -8,6 +8,7 @@ import React from 'react';
 class App extends React.Component{
   constructor(props){
     super(props);
+    this.addTrack = this.addTrack.bind(this);
     this.state = {
       searchResults: [{name: 'name1', artist: "artist1", album:'album1', id: 1},
       {name: 'name2', artist: "artist2", album:'album2', id: 2},
@@ -18,6 +19,23 @@ class App extends React.Component{
       {name: 'name2', artist: "artist2", album:'album2', id: 2},
       {name: 'name3', artist: "artist3", album:'album3', id: 3}]
     }
+
+  }
+
+  removeTrack(track){
+        
+  }
+
+  addTrack(track){
+    const tracks = this.state.playlistTracks;
+    const found = tracks.find(ptrack => ptrack.id === track.id)
+    if (found){
+      return
+    }
+
+    tracks.push(tracks);
+    this.setState({playlistTracks: tracks});
+
   }
 
   render() {
@@ -27,7 +45,7 @@ class App extends React.Component{
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults}/>
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
             <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
           </div>
         </div>
